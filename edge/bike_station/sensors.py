de3029b84ad0c9c0d2be9d835ccc8511ce06b6c4
gesche-grefe <gesche.grefe@grefe-klatt-grefe.de>
2022-06-20 20:59:01 +0200

    Added class for BikeStation. Added a class to represent reservation of spots. Restructured output of main function. Reformatted all files with formatter black.

--- edge/bike_station/sensors.py
+++ edge/bike_station/sensors.py
@@ -2,51 +2,20 @@ import datetime
 import random
 
 
-class BikeSpotSensor:
-    """Virtual sensor for one bike spot.
-    """
-
-    def __init__(self, spot_id):
-        self.spot_id = spot_id
+class SpotOccupiedSensor:
+    def __init__(self):
         self.occupied = bool(random.getrandbits(1))
-        self.bike_battery_sensor = BikeBatterySensor() if self.occupied else None
 
-    def get_spot_sensor_readings(self):
-        """Updates current spot's state and returns sensor readings for current state in dict."""
-        self._update_spot_state()
-        return {
-            "spot_id": self.spot_id,
-            "occupied": self.occupied,
-            "bike_battery_level": self._sense_bike_battery_level()
-        }
-
-    def _update_spot_state(self):
-        """Simulate bike is staying/getting removed/just being parked at empty spot"""
-        new_occupied_state = self._generate_new_occupied_state()
-        if self.occupied and not new_occupied_state:
-            # case bike has been removed
-            self.bike_battery_sensor = None
-        if not self.occupied and new_occupied_state:
-            # case spot was empty and is now taken by new bike
-            self.bike_battery_sensor = BikeBatterySensor()
-        # nothing to change in battery sensor for remaining cases
-        self.occupied = new_occupied_state
-
-    def _generate_new_occupied_state(self):
+    def update_occupied_state(self):
         """Generates new occupied state on the parking spot based on current state.
         If occupied, it is more likely it stays occupied.
         """
         if self.occupied:
             # If spot is occupied, with 65% probability it is stays occupied.
-            return random.random() <= 0.65
+            self.occupied = random.random() <= 0.65
         else:
             # If spot is not occupied, getting occupied is a 50-50 chance
-            return bool(random.getrandbits(1))
-
-    def _sense_bike_battery_level(self):
-        """Returns current battery level of parked bike if spot is occupied."""
-        if self.bike_battery_sensor:
-            return self.bike_battery_sensor.sense_battery_level()
+            self.occupied = bool(random.getrandbits(1))
 
 
 class BikeBatterySensor:
@@ -75,3 +44,15 @@ class BikeBatterySensor:
     def sense_battery_level(self):
         self._update_battery_level()
         return self.battery_level
+
+
+class SolarPanelSensor:
+    """Sensor for production of solar panel."""
+
+    def __init__(self, production_capacity):
+        self.capacity = production_capacity
+        self.current_production = random.randint(0, self.capacity)
+
+    def update_current_production(self):
+        """Update current production with new random value."""
+        self.current_production = random.randint(0, self.capacity)
