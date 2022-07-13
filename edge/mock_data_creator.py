960c82ebb3a07e6f2999c0746f29e0fd91a4f1c0
Samir Teymurov <65294213+samirteymurov@users.noreply.github.com>
2022-07-13 19:55:47 +0200

    Delete mock_data_creator.py

--- edge/mock_data_creator.py
+++
@@ -1,8 +0,0 @@
-import time, random
-from models import SpotSensorData, Status
-
-while True:
-    spot_id = random.randint(0, 5)
-    is_occupied = bool(random.getrandbits(1))
-    sensor_read = SpotSensorData(spot_id=spot_id, is_occupied=is_occupied).add()
-    time.sleep(5)
